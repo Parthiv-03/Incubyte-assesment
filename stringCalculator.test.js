d2e37c7b1936e9ff0e72967ad81ce36c9cdd5952
Parthiv-03 <dhameliyaparthiv55@gmail.com>
2025-07-05 12:44:05 +0530

    return sum for custom delimiterr - passed

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -18,8 +18,12 @@ describe('String Calculator', () => {
             expect(add("1,2,3,4")).toBe(10);
         });
 
-        test('should handle new lines between numbers', () => {
+        it('should handle new lines between numbers', () => {
             expect(add("1\n2,3")).toBe(6);
+        });
+
+        it('should handle custom delimiter', () => {
+            expect(add("//;\n1;2")).toBe(3);
         });
     });
 });
