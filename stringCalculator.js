ccadd303fe9726a6a261acd6ad09155db26bfc2f
Parthiv-03 <dhameliyaparthiv55@gmail.com>
2025-07-05 12:31:10 +0530

    return number for only one number - passed

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,5 +1,22 @@
 function add(numbers){
-    if(numbers == "") return 0;
+    if(numbers === "") return 0;
+
+    let delimiter = "/,|\n/";
+    if(numbers.startsWith("//")){
+        const parts = numbers.split("\n");
+        delimiter = parts[0].substring(2);
+        numbers =parts[1];
+    }
+
+    const nums = numbers.split(new RegExp(`[${delimiter}]`)).map(Number);
+
+    const negatives = nums.filter(n => n<0);
+
+    if(negatives.length > 0){
+        throw new Error('negative numbers not allowed ${negatives.join(",")}');
+    }
+
+    return nums.reduce((sum,n) => sum + n, 0);
 }
 
 module.exports = {add};
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -5,5 +5,9 @@ describe('String Calculator', () => {
         it('should return 0 for empty string', () => {
             expect(add('')).toBe(0);
         });
+        
+        it('should return 1 for single number', () => {
+            expect(add("1")).toBe(1);
+        });
     });
 });
